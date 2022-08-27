a8d3e6b55ce07d645743764c11c478a7889846db
Rock Tsai <r820704@livemail.tw>
2022-08-27 15:32:47 +0800

    修改pom.xml , 可以使用maven install打包成jar並執行啟動

--- src/test/java/com/ff14/domain/FF14ApplicationTests.java
+++ src/test/java/com/ff14/domain/FF14ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.ff14.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class FF14ApplicationTests {
 
 	@Test
 	void contextLoads() {
