38cc521d76509656e7b97405b0c626289c13a3b0
Rock Tsai <r820704@livemail.tw>
2022-08-27 14:02:26 +0800

    Spring boot HelloWorld

---
+++ src/main/java/com/ff14/FF14Application.java
@@ -0,0 +1,17 @@
+package com.ff14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class FF14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FF14Application.class, args);
+	}
+
+	
+
+}
