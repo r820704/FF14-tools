db9efd7a48f910a3bc86e49a981953cb7eae9e12
Rock Tsai <r820704@livemail.tw>
2022-09-03 21:46:12 +0800

    目前local測試可列出所有個人可購買的房屋列表

--- src/main/java/com/ff14/crawler/WikiParser.java
+++ src/main/java/com/ff14/crawler/WikiParser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -35,11 +36,27 @@ public class WikiParser {
 		
 		Thread.sleep(3000);
 		
-		List<WebElement> saleItem = driver.findElements(By.className("sale-item"));
+		List<WebElement> saleItems = driver.findElements(By.className("sale-item"));
 		System.out.println("==========開始列印============");
-		System.out.println(saleItem);
-
+		System.out.println("購買對象: " + saleItems.get(0).findElement
+				(By.cssSelector("span[data-tooltip='仅供部队购买']")).getDomAttribute("data-tooltip"));
 		
+		StringBuilder houseList = new StringBuilder();
+		
+		for (int i = 0; i < saleItems.size(); i++) {
+		    WebElement item = driver.findElements(By.className("sale-item")).get(i);
+		    
+		    try {
+		    	WebElement span = item.findElement(By.cssSelector("span[data-tooltip='仅供个人购买']"));
+		    } catch(NoSuchElementException e) {
+		    	continue;
+		    }
+		    String[] itemContent = item.getText().split("\n");
+		    houseList.append(itemContent[0] + ", " + itemContent[1] + ", " + itemContent[3] + ", "  + itemContent[4] + ", "
+		    		 + itemContent[5] + "\n");
+		}
+		
+		System.out.println("houseList = " + houseList);
 		
 	}
 	
