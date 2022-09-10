2642a6d8fb3fdc93980e206cbf8a3e06a651072b
Rock Tsai <r820704@livemail.tw>
2022-09-10 21:32:04 +0800

    將webdriver初始化時所需的系統參數由hard code改為從application.properties注入

--- src/main/java/com/ff14/crawler/CrawlerConfig.java
+++ src/main/java/com/ff14/crawler/CrawlerConfig.java
@@ -5,6 +5,7 @@ import javax.annotation.PreDestroy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,10 +17,15 @@ public class CrawlerConfig {
 	@Autowired
 	private WebDriver driver;
 	
+	@Value("${webdriver.chrome.driver}")
+	private String WEBDRIVER_PATH ;
+	
 	@Bean
-	public WebDriver driver() {
+	public WebDriver driver(@Value("${webdriver.chrome.driver}") String WEBDRIVER_PATH) {
+
+System.out.println("WEBDRIVER_PATH" + WEBDRIVER_PATH);
 		//若瀏覽器安裝位置為預設則webDriver會自動搜尋path設定的位置，也可以使用System.setProperty 來指定路徑
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", WEBDRIVER_PATH);
 		//Selenium對不同瀏覽器提供了不同的webDriver
 		return new ChromeDriver();
 	}
